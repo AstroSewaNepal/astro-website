d162bf999030c11eb411c6dae62b82faa2cf6405
manoj-aeritech <manojkhatri2073@gmail.com>
2026-05-05 00:19:54 +0545

    build error fixed

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -255,9 +255,9 @@ export async function generateStaticParams() {
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
-  const { slug } = params;
+  const { slug } = await params;
   const post = await getBlogPost(slug);
 
   if (!post) {
