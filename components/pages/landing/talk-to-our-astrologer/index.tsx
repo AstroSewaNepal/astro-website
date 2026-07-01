0e895eb4fec5973f44ddc0bcbbe072d13586c623
SantoshPoudel0 <xpoudel5@gmail.com>
2026-07-01 17:07:28 +0545

    fixing the about us

--- components/pages/landing/talk-to-our-astrologer/index.tsx
+++ components/pages/landing/talk-to-our-astrologer/index.tsx
@@ -34,7 +34,7 @@ const TalkToOurAstrologer: React.FC<TalkToOurAstrologerProps> = ({
       `}</style>
       <section
         className={clsx(
-          'talk-to-our-astrologer-section container mx-auto max-w-full px-3 sm:px-5 lg:px-0',
+          'talk-to-our-astrologer-section container mx-auto max-w-full px-3 sm:px-5 lg:px-0 border-b border-b-[#79787A] pb-[100px]',
           className,
         )}
       >
